fab86706afa67b914cb67424f94bd7c09d2eb07b
kovs705 <smartkovsik@gmail.com>
2023-05-30 14:00:15 +0300

    initial commit

---
+++ Flash Chat iOS13/Model/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Kovs on 29.05.2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
